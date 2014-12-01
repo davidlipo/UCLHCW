56eb873739a8eae0010d0bc1841e0fa8ffda59cd
davidlipo <dlipowicz@hotmail.com>
2014-12-01 18:48:40 +0000

    Move the world instead of the user

--- Assets/Controller.js
+++ Assets/Controller.js
@@ -1,7 +1,8 @@
 ﻿static var speed : int = 100;
+var movingPlane : GameObject;
  
  function Update () {
-     transform.Translate (Vector3(0,0,1) * Time.deltaTime*speed);
+     movingPlane.transform.Translate (Vector3(0,0,-1) * Time.deltaTime*speed);
      if (Input.GetKey (KeyCode.LeftArrow)) transform.Translate (Vector3(-1,0,0) * Time.deltaTime*speed);
      if (Input.GetKey (KeyCode.RightArrow)) transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
  }
--- Assets/mainScript.js
+++ Assets/mainScript.js
@@ -1,5 +1,5 @@
 var coin : GameObject;
-var character : GameObject;
+var movingPlane : GameObject;
 var delay: int;
 
 function Start () {
@@ -7,11 +7,12 @@ function Start () {
 
 function Update () {
 	delay++;
-	if(delay > 100) {
+	if(delay > 60) {
 		delay = 0;
-		var position : Vector3 = new Vector3 (character.transform.position.x + Random.Range(-100,100),30, character.transform.position.z + Random.Range(200,400));
+		var position : Vector3 = new Vector3 (87*Random.Range(0,3) - 57,20, 1000);
 		var rotation : Quaternion = Quaternion.identity;
 		coin.transform.localScale = Vector3(1000,1000,1000);
-		Instantiate (coin, position, rotation);
+		var newCoin = Instantiate (coin, position, rotation);
+		newCoin.transform.parent = movingPlane.transform;
 	}
 }
