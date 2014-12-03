823ec3275cfa22a8a1d038183581ba509b223691
davidlipo <dlipowicz@hotmail.com>
2014-12-03 00:35:42 +0000

    Added in buildings and coins

--- Assets/Controller.js
+++ Assets/Controller.js
@@ -1,8 +1,52 @@
-﻿static var speed : int = 100;
+var coin : GameObject;
+var building : GameObject;
 var movingPlane : GameObject;
- 
- function Update () {
-     movingPlane.transform.Translate (Vector3(0,0,-1) * Time.deltaTime*speed);
-     if (Input.GetKey (KeyCode.LeftArrow)) transform.Translate (Vector3(-1,0,0) * Time.deltaTime*speed);
-     if (Input.GetKey (KeyCode.RightArrow)) transform.Translate (Vector3(1,0,0) * Time.deltaTime*speed);
- }
+
+var numberOfLanes : int = 3;
+
+var leftLanePosition : int = -57;
+var laneWidth = 87;
+
+var distanceToAddCoins : int = 1000;
+var distanceBetweenCoins : int = 200;
+
+var lengthOfBuilding : int = 3406;
+var initialBuildingPosition : int = 1000;
+
+function Start () {
+	var i : int;
+	var rotation : Quaternion = Quaternion.identity;
+	for(i = 0; i < distanceToAddCoins/distanceBetweenCoins; i++){
+		var position : Vector3 = new Vector3 (randomLane(),20, distanceToAddCoins + i*distanceBetweenCoins);
+		coin.transform.localScale = Vector3(1000,1000,1000);
+		var newCoin = Instantiate (coin, position, rotation);
+		newCoin.transform.parent = movingPlane.transform;
+		var coinScript = newCoin.AddComponent('coin');
+		coinScript.controller = gameObject;
+	}
+	
+	//Left
+	for(i = 0; i < 2; i++){
+		position = new Vector3 (-555,-223, initialBuildingPosition + i*lengthOfBuilding);
+		var newBuilding = Instantiate (building, position, rotation);
+		newBuilding.transform.parent = movingPlane.transform;
+		var buildingScript = newBuilding.AddComponent('building');
+		buildingScript.controller = gameObject;
+	}
+	
+	//Right
+	for(i = 0; i < 2; i++){
+		position = new Vector3 (615,-223, initialBuildingPosition + i*lengthOfBuilding);
+		newBuilding = Instantiate (building, position, rotation * Quaternion.Euler(0,180f,0));
+		newBuilding.transform.parent = movingPlane.transform;
+		buildingScript = newBuilding.AddComponent('building');
+		buildingScript.controller = gameObject;
+	}
+}
+
+function Update () {
+}
+
+function randomLane(){
+	return laneWidth*Random.Range(0,3) + leftLanePosition;
+}
---
+++ Assets/coin.js
@@ -0,0 +1,12 @@
+﻿var controller : GameObject;
+
+function Start () {
+}
+
+function Update () {
+	if(transform.position.z < 0) {
+		var controllerScript = controller.GetComponent('controller');
+		transform.position.z = controllerScript.distanceToAddCoins;
+		transform.position.x = controllerScript.randomLane();
+	}
+}
