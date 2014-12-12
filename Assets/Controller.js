3604dac5da0da0244312ba5b2834826ac7460caf
dmasood <danyaalmasood@LAPTOP-2.local>
2014-12-12 13:49:16 +0000

    GameV5
    Game version 5

--- Assets/Controller.js
+++ Assets/Controller.js
@@ -1,6 +1,9 @@
 var coin : GameObject;
 var building : GameObject;
 var movingPlane : GameObject;
+var road : GameObject;
+
+var coinRemoveEffect : ParticleSystem;
 
 var numberOfLanes : int = 3;
 
@@ -13,16 +16,22 @@ var distanceBetweenCoins : int = 200;
 var lengthOfBuilding : int = 3406;
 var initialBuildingPosition : int = 1000;
 
+var initialRoadPosition : int = 800;
+
 function Start () {
 	var i : int;
 	var rotation : Quaternion = Quaternion.identity;
+	var lane : int;
 	for(i = 0; i < distanceToAddCoins/distanceBetweenCoins; i++){
-		var position : Vector3 = new Vector3 (randomLane(),20, distanceToAddCoins + i*distanceBetweenCoins);
-		coin.transform.localScale = Vector3(1000,1000,1000);
+		lane = randomLane();
+		var position : Vector3 = new Vector3 (laneX(lane),20, distanceToAddCoins + i*distanceBetweenCoins);
+		coin.transform.localScale = Vector3(750,750,750);
 		var newCoin = Instantiate (coin, position, rotation);
 		newCoin.transform.parent = movingPlane.transform;
 		var coinScript = newCoin.AddComponent('coin');
 		coinScript.controller = gameObject;
+		coinScript.lane = lane;
+		coinScript.removeEffect = coinRemoveEffect;
 	}
 	
 	//Left
@@ -42,11 +51,23 @@ function Start () {
 		buildingScript = newBuilding.AddComponent('building');
 		buildingScript.controller = gameObject;
 	}
+	
+	for(i = 0; i < 2; i++){
+		position = new Vector3 (30,1, initialRoadPosition + i*road.renderer.bounds.size.z);
+		var newRoad = Instantiate (road, position, rotation);
+		newRoad.transform.parent = movingPlane.transform;
+		var roadScript = newRoad.AddComponent('road');
+		roadScript.controller = gameObject;
+	}
 }
 
 function Update () {
 }
 
 function randomLane(){
-	return laneWidth*Random.Range(0,3) + leftLanePosition;
+	return Random.Range(0,3);
+}
+
+function laneX(laneNumber : int){
+	return laneWidth*laneNumber + leftLanePosition;
 }
