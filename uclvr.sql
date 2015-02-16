cc091ba91d02dcc7dfc72a3c0e63dcb31b66b329
davidlipo <dlipowicz@hotmail.com>
2015-02-16 17:08:08 +0000

    Updated file

--- uclvr.sql
+++ uclvr.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 16, 2015 at 04:57 PM
+-- Generation Time: Feb 16, 2015 at 05:07 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.12
 
@@ -31,9 +31,17 @@ USE `uclvr`;
 CREATE TABLE IF NOT EXISTS `attempts` (
   `attemptID` int(4) NOT NULL AUTO_INCREMENT,
   `patientID` int(5) NOT NULL,
-  `time` date NOT NULL,
+  `time` datetime NOT NULL,
+  `level` int(3) NOT NULL,
   PRIMARY KEY (`attemptID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `attempts`
+--
+
+INSERT INTO `attempts` (`attemptID`, `patientID`, `time`, `level`) VALUES
+(1, 1, '2015-02-16 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -43,14 +51,23 @@ CREATE TABLE IF NOT EXISTS `attempts` (
 
 CREATE TABLE IF NOT EXISTS `patientstats` (
   `ID` int(5) NOT NULL AUTO_INCREMENT,
-  `level` int(3) NOT NULL,
   `attemptID` int(4) NOT NULL,
   `type` varchar(10) NOT NULL,
   `laneNo` int(2) NOT NULL,
   `collected` int(1) NOT NULL,
   `timeTaken` float NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `patientstats`
+--
+
+INSERT INTO `patientstats` (`ID`, `attemptID`, `type`, `laneNo`, `collected`, `timeTaken`) VALUES
+(1, 1, 'coin', 1, 1, 2),
+(2, 1, 'coin', 2, 1, 4),
+(3, 1, 'coin', -1, 0, 0),
+(4, 1, 'coin', -2, 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
