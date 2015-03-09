ef15053a01ce1472c2626d4c50df9ad6f73d4ee4
davidlipo <dlipowicz@hotmail.com>
2015-03-09 22:07:24 +0000

    Find out what levels the user should play next
    Pass in attemptID, returns leftLevel+rightLevel

---
+++ getLevels.php
@@ -0,0 +1,46 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "", "uclvr");
+
+$attemptID = $_GET['attemptID'];
+
+$arrow = ">";
+
+$queryAttempt = mysqli_query($link, "SELECT * FROM attempts WHERE attemptID = '$attemptID'");
+$resAttempt = mysqli_fetch_assoc($queryAttempt);
+$level = $resAttempt['level'];
+
+$leftLevel = $level + levelsToProgress($level, "<");
+$rightLevel = $level + levelsToProgress($level, ">");
+
+echo $leftLevel . "+" . $rightLevel;
+
+function levelsToProgress($level, $arrow) {
+	global $link, $attemptID;
+	$query = mysqli_query($link, "SELECT * FROM patientStats WHERE attemptID = '$attemptID' AND type = 'coin' AND laneNo $arrow '0'");
+
+	$total = 0;
+	$size = 0;
+
+	while($res = mysqli_fetch_assoc($query)) {
+		if($res['collected'] == 1) {
+			$total += 1 - min(($res['timeTaken']*$level)/8, 1);
+		}
+		$size++;
+	}
+
+	$progress = $total / $size;
+
+	if ($progress > 0.6) {
+		return 3;
+	}
+	else if ($progress > 0.4) {
+		return 2;
+	}
+	else if ($progress > 0.2) {
+		return 1;
+	}
+	return 0;		
+}
+
+?>
