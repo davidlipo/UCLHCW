241d3e6d87ae2c450e7ea254f94812921e3c8194
ilya0701 <ilya0701@mail.ru>
2015-03-12 13:05:52 +0000

    Added patientID search field and level drop down box to analyse page
    The dropdown box should have the levels atempted by the patient defined
    by the ID that is searched. It is not working for me atm but it may be
    because I've setup the database incorrectly (the graph does not work as
    well). If one of you could try it and lmk if it functions correctly,
    that would be great

--- Doctor Interface/analyse.php
+++ Doctor Interface/analyse.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html>
 
+    <script>
+        function addOptions(x){
+            var select = document.getElementById('dropdown');
+            select.options[select.options.length] = new Option(x, x);   
+        }
+    
+    </script>
+    
     <head>
 	<link rel="stylesheet" href="boilerplate.css">
 	<link rel="stylesheet" href="analyse.css">
@@ -14,7 +22,31 @@
         <img id="image1" src="img/uclh%20(1).png" class="image" />
 		
 		<img src="bargraph.php">
-		
+        
+        <div id="formContainer" class="formContainer clearfix">
+            <form id= "form">
+                Patient ID:<br>
+                <input type="text" name="patientID">
+                <input id="smallButton" type="Submit" value="Find">
+            </form>
+            <?php
+                if(isset($_POST['submit'])){ 
+                    $PatientID=$_POST['patientID']; 
+                    $sql_connect = new mysqli("localhost", "root", "") or die ("no DB Connection");
+                    mysqli_select_db($sql_connect, "uclvr") or die ("DB not found"); 
+                    $sql="SELECT level WHERE patientID=$PatientID FROM attempts";
+                    $result=mysql_query($sql);
+                    while($row=mysql_fetch_array($result)){ 
+                        runlist($row['level'];
+                    }
+            ?>
+        </div>
+        
+        <div id="formContainer" class="formContainer clearfix">
+            <select name="levels" id="dropdown"> 
+            </select>    
+        </div>
+        
         <a href="home.html">
             <input id="input" type="button" value="Main menu"></input>
         </a>
